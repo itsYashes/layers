9b915cd42a2fcbaf0dae3eddd2d004c0ee37a7f2
itsYashes <yashasravindra007@gmail.com>
2025-04-12 19:35:15 +0530

    Refactor Footer component

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,4 +1,4 @@
-import logoImage from "@/assets/images/logo.svg"
+import logoImage from "@/assets/images/logo.svg";
 import Image from "next/image";
 
 const footerLinks = [
@@ -8,20 +8,28 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <section className="py-16">
-        <div className="container">
-            <div className="flex flex-col md:flex-row items-center md:justify-between gap-6">
-                <div>
-                    <Image src={logoImage} alt="Layers logo" />
-                </div>
-                <div>
-                    <nav className="flex gap-6">
-                        {footerLinks.map(link => (
-                            <a href={link.href} className="text-white/50 text-sm">{link.label}</a>
-                        ))}
-                    </nav>
+    return (
+        <section className="py-16">
+            <div className="container">
+                <div className="flex flex-col md:flex-row items-center md:justify-between gap-6">
+                    <div>
+                        <Image src={logoImage} alt="Layers logo" />
+                    </div>
+                    <div>
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <a
+                                    key={link.label}
+                                    href={link.href}
+                                    className="text-white/50 text-sm"
+                                >
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>;
+        </section>
+    );
 }
