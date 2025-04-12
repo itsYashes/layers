d114e81aeb58d1920be9b9c31e4158968d5e00d4
itsYashes <yashasravindra007@gmail.com>
2025-04-12 19:08:48 +0530

    Add new files and components

---
+++ src/components/Button.tsx
@@ -0,0 +1,29 @@
+import { ButtonHTMLAttributes } from 'react';
+import { cva } from 'class-variance-authority';
+
+const classes = cva('border  rounded-full h-12 px-6 font-medium', {
+    variants: {
+        variant: {
+            primary: 'bg-lime-400 text-neutral-950 border-lime-400',
+            secondary: 'border-white text-white bg-transparent',
+        },
+        size: {
+            sm: "h-10"
+        }
+    }
+})
+
+export default function Button(
+    props: { variant: "primary" | "secondary"; size?: 'sm';} & ButtonHTMLAttributes<HTMLButtonElement>) {
+    const {variant, size, className, ...otherProps} = props;
+    return (
+        <button 
+            className={classes({
+                variant, 
+                size,
+                className
+            })}
+            {...otherProps}
+        />
+    );
+}
